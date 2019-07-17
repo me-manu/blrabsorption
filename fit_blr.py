0cf6bf4e37372bc28c5becdd50036fdc6fa4a264
me-manu <me.manu@gmx.net>
2019-07-17 10:01:38 +0200

    Added possibility for chi2 fit

--- fit_blr.py
+++ fit_blr.py
@@ -24,12 +24,12 @@ def setDefault(func = None, passed_kwargs = {}):
     Read in default keywords of the simulation and pass to function
     """
     if func is None:
-	return functools.partial(setDefault, passed_kwargs = passed_kwargs)
+        return functools.partial(setDefault, passed_kwargs = passed_kwargs)
     @functools.wraps(func)
     def init(*args, **kwargs):
-	for k in passed_kwargs.keys():
-	    kwargs.setdefault(k,passed_kwargs[k])
-	return func(*args, **kwargs)
+        for k in passed_kwargs.keys():
+            kwargs.setdefault(k,passed_kwargs[k])
+        return func(*args, **kwargs)
     return init
 # ---------------------------------------------------------- #
 
@@ -39,94 +39,132 @@ def setDefault(func = None, passed_kwargs = {}):
 #is less than 0.001*tolerance*UP (see SET ERR). 
 
 minuit_def = {
-	'verbosity': 0,	
-	'int_steps': 1e-4,
-	'strategy': 2,
-	'tol': 1e-5,
-	'up': 0.5,
-	'max_tol_increase': 3000.,
-	'tol_increase': 1000.,
-	'ncall': 5000,
-	'r_steps': 40,
-	'scan_bound': (16.,18.),
-	'pedantic': True,
-	'precision': None,
-	'scipy': True,
-	'pinit': {'norm' : -10.,
-		'index': -3.,
-		'alphac': 1.,
-		'r': 17.},
-	'fix': {'norm' : False,
-		'index': False,
-		'alphac': False,
-		'r': False },
-	'limits': {'norm' : [-20,-5],
-		'index': [-5,5],
-		'alphac': [0.1,10.],
-		'r': [16.,18.]},
-	'islog': {'norm' : True,
-		'index': False,
-		'alphac': False,
-		'r': True},
-	}
+        'verbosity': 0,        
+        'int_steps': 1e-4,
+        'strategy': 2,
+        'tol': 1e-5,
+        'up': 0.5,
+        'max_tol_increase': 3000.,
+        'tol_increase': 1000.,
+        'ncall': 5000,
+        'r_steps': 40,
+        'scan_bound': (16.,18.),
+        'pedantic': True,
+        'precision': None,
+        'scipy': True,
+        'pinit': {'norm' : -10.,
+                'index': -3.,
+                'alphac': 1.,
+                'r': 17.},
+        'fix': {'norm' : False,
+                'index': False,
+                'alphac': False,
+                'r': False },
+        'limits': {'norm' : [-20,-5],
+                'index': [-5,5],
+                'alphac': [0.1,10.],
+                'r': [16.,18.]},
+        'islog': {'norm' : True,
+                'index': False,
+                'alphac': False,
+                'r': True},
+        }
 # --- miniuit defaults ------------------------------------- #
 
 class FitBLR(object):
     def __init__(self, llh, taublr, EGeVllh, EGeVtau, fluxllh, rtau,
-    		kx = 2, ky = 2):
+                    kx = 2, ky = 2, fit_mode = 'mle', covar = None):
         """
         Initialize the class
 
         Parameters
         ----------
         llh: `~numpy.ndarray`
-            n x m dimensional cube, dimension n: energy bins, dimension m: flux bins, 
-            each entry gives the log likelihood value for that energy bin and flux
+            if fit_mode = 'mle':
+                n x m dimensional cube, dimension n: energy bins, dimension m: flux bins,
+                each entry gives the log likelihood value for that energy bin and flux
+            else if fit_mode = 'chi2':
+                flux measurements
 
         taublr: `~numpy.ndarray`
             i x k dimensional cube with optical depths for i energies and k distances
 
         EGeVllh:`~numpy.ndarray`
-            n dimensional array with central bin energies for llh cube
+            if fit_mode = 'mle':
+                n dimensional array with central bin energies for llh cube
+            else if fit_mode = 'chi2':
+                energies corresponding to the flux measurements
 
         EGeVtau:`~numpy.ndarray`
             i dimensional array with central bin energies for tau cube
 
         flux_llh:`~numpy.ndarray`
-            m dimensional array with central flux bin values for llh cube
-            or n x m dimensional array with central flux bin values for llh cube
-            for each energy bin
-            
+            if fit_mode = 'mle':
+                m dimensional array with central flux bin values for llh cube
+                or n x m dimensional array with central flux bin values for llh cube
+                for each energy bin
+            else if fit_mode = 'chi2':
+                the flux measurement errors
+
         rtau :`~numpy.ndarray`
             k dimensional array with central distance bin values for tau cube
+
+        covar: array-like
+            covariance matrix of flux measurements, only used if fit_mode = 'chi2'
+
+        fit_mode: str
+            either 'mle' or 'chi2'. If 'mle' a maximum likelihood estimate is performed and
+            a least square fit otherwise.
         """
         self._taublr = taublr
-        self._EGeVllh = EGeVllh
-        self._fluxllh = fluxllh
         self._EGeVtau = EGeVtau
         self._rtau = rtau
-        self._llh = llh
-        self._llh_intp = []
+        self._profile_llh = None
+        self._profile_par_names = None
+        self._profile_scale = None
+
+        self._scale_name = None
+        self._index_name = None
+        self._norm_name = None
+        self._fit_mode = fit_mode
+
+        self._EGeVllh = EGeVllh
+        self._fit_mode = fit_mode
+        if fit_mode == 'mle':
+            self._fluxllh = fluxllh
+            self._llh = llh
+            self._llh_intp = []
+
+            # piece-wise interpolation of llh
+            if len(self._fluxllh.shape) == 1:
+                self._fluxllh[self._fluxllh == 0.] = 1e-40 * np.ones(np.sum(self._fluxllh == 0.))
+                for i, l in enumerate(self._llh):
+                    self._llh_intp.append(USpline(np.log(self._fluxllh),l,
+                                              k = 2, s = 0, ext = 'extrapolate'))
+            elif len(self._fluxllh.shape) == 2:
+                for i, l in enumerate(self._llh):
+                    self._fluxllh[i][self._fluxllh[i] == 0.] = \
+                        1e-40 * np.ones(np.sum(self._fluxllh[i] == 0.))
+                    self._llh_intp.append(USpline(np.log(self._fluxllh[i]),l,
+                                              k = 2, s = 0, ext = 'extrapolate'))
+        elif fit_mode == 'chi2':
+            self._y = llh
+            self._dy = fluxllh
+            if covar is None:
+                self._cov_inv = None
+            else:
+                self._cov_inv = np.linalg.inv(covar)
+        else:
+            raise ValueError("Unknown fit mode chosen, must be either 'mle' or 'chi2'")
 
 # interpolate taublr
         self.__tauSpline = RBSpline(np.log(EGeVtau),np.log(rtau),self._taublr,kx=kx,ky=ky)
 
-# piece-wise interpolation of llh 
-        for i,l in enumerate(self._llh):
-            if len(self._fluxllh.shape) == 1:
-                self._fluxllh[i][self._fluxllh == 0.] = \
-                    1e-40 * np.ones(np.sum(self._fluxllh == 0.))
-                self._llh_intp.append(USpline(np.log(self._fluxllh),l,
-                    k = 2, s = 0, ext = 'extrapolate'))
-            elif len(self._fluxllh.shape) == 2:
-                self._fluxllh[i][self._fluxllh[i] == 0.] = \
-                    1e-40 * np.ones(np.sum(self._fluxllh[i] == 0.))
-                self._llh_intp.append(USpline(np.log(self._fluxllh[i]),l,
-                    k = 2, s = 0, ext = 'extrapolate'))
-        return 
+        return
 
     @staticmethod
-    def readfermised(sedfile, taublr, EGeVtau, rtau, hdu = 1, tsmin = None, emin = None, emax = None, fit_qual = False):
+    def readfermised(sedfile, taublr, EGeVtau, rtau,
+                    hdu = 1, tsmin = None, emin = None, emax = None, fit_qual = False):
         """
         Initialize the class by building a data likelihood cube 
         from a fermipy SED fits file
@@ -195,7 +233,7 @@ class FitBLR(object):
 
     @staticmethod
     def readxydata(x,y,dy, mUL, taublr, EGeVtau, rtau, scale = 5., ysteps = 400, ymin = 1e-15, 
-    			ULconf = 0.95, distr = 'norm'):
+                            ULconf = 0.95, distr = 'norm', fit_mode = 'mle', covar = None):
         """
         Initialize the class by building a data likelihood cube 
         from x (EGeV) and y (dN/dE) data with statistical error dy
@@ -236,28 +274,131 @@ class FitBLR(object):
         
         ULconf: float
             confidence interval for upper limit
+
+        fit_mode: str
+            either 'mle' for maximum likelihood estimation or 'chi2' for least square fitting
+
+        covar: array-like
+            covariance matrix for y measurements (only used if fit_mode = 'chi2')
         """
-        ymin = np.min((ymin, np.min(np.abs(y - scale * dy))))
-        ymax = np.max(y + scale * dy)
-        yrange = np.logspace(np.log10(ymin), np.log10(ymax), ysteps)
+        if fit_mode == 'mle':
+            ymin = np.min((ymin, np.min(np.abs(y - scale * dy))))
+            ymax = np.max(y + scale * dy)
+            yrange = np.logspace(np.log10(ymin), np.log10(ymax), ysteps)
 
-        llh = []
-        for i,E in enumerate(x):
-            if mUL[i]:
-                llh.append(norm.pdf(yrange, loc = ymin, scale = dy[i] / ndtri(ULconf)))
-                print "assuming {0:.3f} confidence level".format(
-                    erfnorm(dy[i], 0.,0., dy[i] / ndtri(ULconf)) + 0.5
-                    )
-            else:
-                if distr == 'norm':
-                    llh.append(norm.pdf(yrange, loc = y[i], scale = dy[i]))
-                elif distr == 'lognorm':
-                    sigma = np.sqrt(np.log(1. + dy[i]**2. / y[i]**2.))
-                    llh.append(lognorm.pdf(yrange / y[i], sigma))
+            llh = []
+            for i,E in enumerate(x):
+                if mUL[i]:
+                    llh.append(norm.pdf(yrange, loc = ymin, scale = dy[i] / ndtri(ULconf)))
+                    cl = erfnorm(dy[i], 0.,0., dy[i] / ndtri(ULconf)) + 0.5
+                    if np.isnan(cl):
+                        raise RuntimeError("CL is NaN!")
+                    print ("assuming {0:.3f} confidence level".format(cl))
+                else:
+                    if distr == 'norm':
+                        llh.append(norm.pdf(yrange, loc = y[i], scale = dy[i]))
+                    elif distr == 'lognorm':
+                        sigma = np.sqrt(np.log(1. + dy[i]**2. / y[i]**2.))
+                        llh.append(lognorm.pdf(yrange / y[i], sigma))
 
-            llh[-1][llh[-1] < 1e-200] = np.ones(np.sum(llh[-1] < 1e-200)) * 1e-200
+                llh[-1][llh[-1] < 1e-200] = np.ones(np.sum(llh[-1] < 1e-200)) * 1e-200
 
-        return FitBLR(np.log(llh), taublr, x, EGeVtau, yrange, rtau)
+            return FitBLR(np.log(llh), taublr, x, EGeVtau, yrange, rtau, fit_mode = 'mle')
+        elif fit_mode == 'chi2':
+            return FitBLR(y, taublr, x, EGeVtau, dy, rtau, fit_mode = 'chi2', covar = covar)
+        else:
+            raise ValueError("Unknown fitting mode chosen. Either 'chi2' or 'mle'")
+
+    def add_llhprofile(self,profile, x, y,
+        xname = 'Index', yname = 'Prefactor',
+        scale = 1.,
+        scale_name = 'Scale',
+        index_name = 'Index',
+        norm_name = 'Prefactor',
+        logx = False,
+        logy = False,
+        **kwargs):
+        """
+        Add a 2d likelihood surface for model parameters
+
+        Parameters
+        ----------
+        profile: array-like
+            n x m dimensional array with profile likelihood
+
+        x: array-like
+            n dimensional array, x values at which likelihood is evaluated
+
+        y: array-like
+            m dimensional array, y values at which likelihood is evaluated
+
+        {options}
+
+        xname: str
+            parameter name of x values. default: "Index" (i.e. power-law index)
+
+        yname: str
+            parameter name of y values. default: "Prefactor" (i.e. power-law index)
+
+        logx: bool
+            if True, interpolate x as log10
+
+        logy: bool
+            if True, interpolate y as log10
+
+        scale: float
+            if one parameter is a prefactor, this is the Scale (pivot energy) that was
+            used when likelihood was extracted. Default: 1.
+
+        scale_name: str
+            parameter name for scale, Default: "Scale"
+
+        index_name: str
+            parameter name for index, Default: "Index"
+
+        norm_name: str
+            parameter name for normalization, Default: "Prefactor"
+
+        kwargs are passed to Rectilinear 2D Interpolation
+
+        Notes:
+        ------
+        Profile likelihood will be added to full likelihood, even if it is derived
+        for a limited energy range. You have to make sure during the fit by contraining
+        fit parameters that the energy range is correct
+        """
+        kwargs.setdefault('kx', 2)
+        kwargs.setdefault('ky', 2)
+
+        self._scale_name = scale_name
+        self._index_name = index_name
+        self._norm_name = norm_name
+
+        if logx:
+            xint = np.log10(x)
+        else:
+            xint = x
+        if logy:
+            yint = np.log10(y)
+        else:
+            yint = y
+
+        profile_int  = RBSpline(xint, yint, profile, **kwargs)
+
+        if logx and logy:
+            self._profile_llh = lambda x,y : profile_int(np.log10(x),np.log10(y))
+        elif logx:
+            self._profile_llh = lambda x,y : profile_int(np.log10(x),y)
+        elif logy:
+            self._profile_llh = lambda x,y : profile_int(x,np.log10(y))
+        else:
+            self._profile_llh = lambda x,y : profile_int(x,y)
+
+        self._profile_par_names = [xname, yname]
+        self._profile_scale = scale
+
+        return
+
 
     def opt_depth(self,rcm,EGeV):
         """
@@ -293,13 +434,13 @@ class FitBLR(object):
         args_E = np.argsort(EGeV)
 
         # Spline interpolation requires sorted lists
-        tt[args_r,:] = self.__tauSpline(np.log(np.sort(EGeV)),np.log(np.sort(rcm))).transpose()	
+        tt[args_r,:] = self.__tauSpline(np.log(np.sort(EGeV)),np.log(np.sort(rcm))).transpose()        
         result[:,args_E] = tt
 
         return np.squeeze(result)
 
     def calcLikelihood(self,*args):
-    	return self.__calcLikelihood(*args)
+            return self.__calcLikelihood(*args)
 
     def __calcLikelihood(self,*args):
         """
@@ -338,13 +479,54 @@ class FitBLR(object):
         f = self._int_spec(self._EGeVllh, **params)
         tau = self.opt_depth(params['r'],self._EGeVllh)
 
-        llh = 0.
-        self.llh_pw = np.zeros(self._EGeVllh.shape[0])
-        for i,x in enumerate(self._EGeVllh):
-            llh += self._llh_intp[i](np.log(f[i]) - tau[i])
-            self.llh_pw[i] = self._llh_intp[i](np.log(f[i]) - tau[i])
+
+        if self._fit_mode == 'mle':
+            llh = 0.
+            self.llh_pw = np.zeros(self._EGeVllh.shape[0])
+            for i, x in enumerate(self._EGeVllh):
+                llh += self._llh_intp[i](np.log(f[i]) - tau[i])
+                self.llh_pw[i] = self._llh_intp[i](np.log(f[i]) - tau[i])
+
+        elif self._fit_mode =='chi2':
+            if self._cov_inv is None:
+                llh = -1. * ((self._y - f * np.exp(-tau)) ** 2. / self._dy ** 2.).sum()
+                self.llh_pw =  ((self._y - f * np.exp(-tau)) ** 2. / self._dy ** 2.)
+            else:
+                llh = -1. * np.dot(self._y - f * np.exp(-tau), np.dot(self._cov_inv, self._y - f * np.exp(-tau)))
+                self.llh_pw =  np.dot(self._y - f * np.exp(-tau), np.dot(self._cov_inv, self._y - f * np.exp(-tau)))
+
+        # add contribution from profile likelihood
+        if self._profile_llh is not None:
+            if self._fit_mode == 'chi2':
+                llh += 2. * self.eval_profile2d(params)
+            else:
+                llh += self.eval_profile2d(params)
 
         return -1. * llh
+
+
+    def eval_profile2d(self, params):
+        """Evaluate 2D profile likelihood """
+
+        try:
+            if self._norm_name in self._profile_par_names:
+                idx_norm = self._profile_par_names.index(self._norm_name)
+            else:
+                idx_norm = None
+
+            scale = []
+            for i, p in enumerate(self._profile_par_names):
+                if idx_norm is not None and i == idx_norm:
+                    scale.append(np.power(self._profile_scale / params[self._scale_name],
+                                          params[self._index_name]))
+                else:
+                    scale.append(1.)
+            profilellh = self._profile_llh(params[self._profile_par_names[0]] * scale[0],
+                   params[self._profile_par_names[1]] * scale[1])
+        except NameError:
+            raise NameError("You need initialize 2D profile likelihood first!")
+
+        return profilellh
 
     @setDefault(passed_kwargs = minuit_def)
     def fill_fitarg(self, **kwargs):
@@ -372,6 +554,10 @@ class FitBLR(object):
         Initial parameters are estimated with scipy fit.
         """
         self.fitarg = fitarg
+        if self._fit_mode == 'mle':
+            kwargs['up'] = 0.5
+        else:
+            kwargs['up'] = 1.
 
         values, bounds = [],[]
         for k in self.parnames:
@@ -392,7 +578,7 @@ class FitBLR(object):
                 options={'maxiter': kwargs['ncall']} #'xtol': 1e-20, 'eps' : 1e-20, 'disp': True}
                 #tol=None, callback=None, 
                 #options={'disp': False, 'minfev': 0, 'scale': None, 
-		    		#'rescale': -1, 'offset': None, 'gtol': -1, 
+                                    #'rescale': -1, 'offset': None, 'gtol': -1, 
                     #'eps': 1e-08, 'eta': -1, 'maxiter': kwargs['ncall'], 
                     #'maxCGit': -1, 'mesg_num': None, 'ftol': -1, 'xtol': -1, 'stepmx': 0, 
                     #'accuracy': 0}
@@ -403,12 +589,12 @@ class FitBLR(object):
 
             logging.debug(fitarg)
         cmd_string = "lambda {0}: self.__calcLikelihood({0})".format(
-					(", ".join(self.parnames), ", ".join(self.parnames)))
+                                        (", ".join(self.parnames), ", ".join(self.parnames)))
 
         string_args = ", ".join(self.parnames)
         global f # needs to be global for eval to find it
         f = lambda *args: self.__calcLikelihood(*args)
-	
+        
         cmd_string = "lambda %s: f(%s)" % (string_args, string_args)
         logging.debug(cmd_string)
 
@@ -534,14 +720,14 @@ class FitBLR(object):
         logging.debug(self.m.fval)
 
         fmin = self.m.get_fmin()
-	
+        
 
         if not fmin.hesse_failed:
             try:
                 self.corr = self.m.np_matrix(correlation=True)
             except:
                 self.corr = -1
-	
+        
         logging.debug(self.m.values)
 
         if self.m.migrad_ok():
@@ -568,7 +754,6 @@ class FitBLR(object):
                         fitarg[k] = bf[idx][k]
                     fitarg['fix_r'] = True
                     kwargs['scipy'] = False
-                    print fitarg
                     self.run_migrad(fitarg, **kwargs)
 
             if minos:
